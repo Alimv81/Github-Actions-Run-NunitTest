e37cbc0f441b7be583eae7a78346c3269a7c60d7
Ali Mohammadi Variani <ali.m.v.13811381@gmail.com>
2024-10-16 09:39:49 +0330

    fixed add function

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,7 @@
 
 public class Calculator
 {
-    public double Add(double a, double b) => a + b + 5;
+    public double Add(double a, double b) => a + b;
     public double Subtract(double a, double b) => a - b;
     public double Multiply(double a, double b) => a * b;
     public double Divide(double a, double b)
